6bf412fadfd4b61f1554a4e887e1866c52f94556
Nikita Sokolov <mr.sokolov99@gmail.com>
2023-01-20 12:15:09 +0300

    homework done 2

--- src/middlewares/files.js
+++ src/middlewares/files.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, path.join(__dirname, "../uploads"));
+    cb(null, path.join(__dirname, "../../uploads"));
   },
   filename: (_, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname.replace(" ", "_")}`);
